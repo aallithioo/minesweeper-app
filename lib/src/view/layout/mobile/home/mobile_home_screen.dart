eab8454e428387c5ac3d41086da9d755d6d74f15
Thio Alli <connect.thioalli@gmail.com>
2022-03-10 17:44:43 +0700

    refactor (mobile) : add grid - home/

--- lib/src/view/layout/mobile/home/mobile_home_screen.dart
+++ lib/src/view/layout/mobile/home/mobile_home_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:minesweeper/src/app/theme/color.dart';
-import 'package:minesweeper/src/app/theme/font_weight.dart';
-import 'package:minesweeper/src/app/widget/border.dart';
-import 'package:minesweeper/src/app/widget/cross_axis_alignment.dart';
-import 'package:minesweeper/src/app/widget/main_axis_alignment.dart';
+import 'package:minesweeper/src/app/theme/letter_spacing.dart';
+import 'package:minesweeper/src/app/widget/margin.dart';
+import 'package:minesweeper/src/app/widget/padding.dart';
 
+import '../../../../app/theme/color.dart';
+import '../../../../app/theme/font_size.dart';
+import '../../../../app/theme/font_weight.dart';
+import '../../../../app/theme/size.dart';
 import '../../../../app/theme/theme.dart';
+import '../../../../app/widget/border.dart';
+import '../../../../app/widget/cross_axis_alignment.dart';
+import '../../../../app/widget/main_axis_alignment.dart';
 
 part './widget/mobile_home_body.dart';
 part './widget/mobile_home_content.dart';
--- lib/src/view/layout/mobile/home/widget/mobile_home_content.dart
+++ lib/src/view/layout/mobile/home/widget/mobile_home_content.dart
@@ -12,33 +12,98 @@ class _MobileHomeContentState extends State<MobileHomeContent> {
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: SetMainAxisAlign.spaceBetween,
+      crossAxisAlignment: SetCrossAxisAlign.center,
       children: [
         // TODO : add a game stat
         Container(
-          height: MediaQuery.of(context).size.height * 0.3,
+          height: MediaQuery.of(context).size.height * 0.2,
           width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
-            color: SetColor.light['blue'],
-            borderRadius: SetBorder.bottom['sm'],
+            color: SetColor.tertiary.withOpacity(0.7),
           ),
           child: Row(
-            mainAxisAlignment: SetMainAxisAlign.center,
+            mainAxisAlignment: SetMainAxisAlign.spaceEvenly,
             crossAxisAlignment: SetCrossAxisAlign.center,
             children: [
               // TODO : add total bomb
-              Container(),
+              Column(
+                mainAxisAlignment: SetMainAxisAlign.center,
+                children: [
+                  Text(
+                    '06',
+                    style: theme.textTheme.headline5!.copyWith(
+                      color: SetColor.primary,
+                      fontSize: SetFontSize.lg / 1.5,
+                      fontWeight: SetFontWeight.light,
+                    ),
+                  ),
+                  Text(
+                    'bombs'.toUpperCase(),
+                    style: theme.textTheme.bodyText1!.copyWith(
+                      color: SetColor.secondary,
+                      letterSpacing: SetLetterSpacing.positive * 2.0,
+                    ),
+                  ),
+                ],
+              ),
               // TODO : add restart game
               IconButton(
-                icon: Icon(Icons.refresh),
+                icon: Icon(
+                  Icons.refresh_outlined,
+                  size: SetSize.md,
+                  color: SetColor.primary,
+                ),
                 onPressed: () {},
               ),
               // TODO : add total win game
-              Container(),
+              Column(
+                mainAxisAlignment: SetMainAxisAlign.center,
+                children: [
+                  Text(
+                    '00',
+                    style: theme.textTheme.headline5!.copyWith(
+                      color: SetColor.primary,
+                      fontSize: SetFontSize.lg / 1.5,
+                      fontWeight: SetFontWeight.light,
+                    ),
+                  ),
+                  Text(
+                    'Win'.toUpperCase(),
+                    style: theme.textTheme.bodyText1!.copyWith(
+                      color: SetColor.secondary,
+                      letterSpacing: SetLetterSpacing.positive * 2.0,
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
 
         // TODO : add grid
+        Expanded(
+          child: Padding(
+            padding: SetPadding.all['sm'],
+            child: GridView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 10,
+              ),
+              itemCount: 100,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: SetPadding.all['xs'] / 4.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: SetColor.tertiary.withOpacity(0.3),
+                      borderRadius: SetBorder.all['xs'] / 2.0,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
 
         // TODO : add brand
         Text(
